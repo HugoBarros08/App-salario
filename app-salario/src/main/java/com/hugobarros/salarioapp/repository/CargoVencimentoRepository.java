4ad6a00009838dbbf0953954fd2b00bea2f856db
Hugo Barros <hugo.barros@ufrn.br>
2025-10-31 19:46:02 -0300

    feat: adicionar funcionalidade para cálculo de salários (telas, bean, service e repositories)

--- app-salario/src/main/java/com/hugobarros/salarioapp/repository/CargoVencimentoRepository.java
+++ app-salario/src/main/java/com/hugobarros/salarioapp/repository/CargoVencimentoRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CargoVencimentoRepository extends JpaRepository<CargoVencimento, Integer> {
+
+    List<CargoVencimento> findByCargoId(Integer cargoId);
 }
